72222c817271ec317a880b6bcbfaaf4c2eecb96d
Christopher <thorstedc266935@gmail.com>
2017-08-02 14:09:58 -0500

    pushing to gh-pages

--- noServCtrl.js
+++ noServCtrl.js
@@ -1,4 +1,14 @@
 angular.module('myApp').controller('nSCtrl', function($scope, $http, $log, myServ) {
+  angular.element(document).ready(function() {
+    $('p.contact').fadeIn('4000');
+    $('h1').fadeIn('6000');
+    $('#disappear').on('click', function() {
+      console.log("disappear");
+      $('.submit').css('display', 'none')
+      $('.thanks').css('display', 'block')
+    })
+
+  })
 
 
 $scope.identity = myServ.identity().then(function(response) {
--- scripts.js
+++ scripts.js
@@ -1,9 +1,1 @@
-$(document).ready(function() {
-  $(this).find('p.contact').fadeIn('4000');
-  $(this).find('h1').fadeIn('6000');
-  $('.disappear').on('click', function() {
-    $('.submit').css('display', 'none')
-    $('.thanks').css('display', 'block')
-  })
 
-})
